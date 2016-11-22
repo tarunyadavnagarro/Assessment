c89d52032f493144a142344d1983bd6482fa53c4
tarunyadvanagarro <tarun.yadav@nagarro.com>
2016-11-22 15:11:17 +0530

    general failure case update

--- sliceFunc/slice_test.go
+++ sliceFunc/slice_test.go
@@ -1,13 +1,25 @@
-package sliceFunc
-import "testing"
+package main
+
+import (
+	"testing"
+)
+
 func TestSLiceFunc(t *testing.T) {
 
 	val := Slicer(-1)
 	if val != true {
 
-		t.Fatal("failed")
+		t.Fatal("failed for negative")
 
 	}
 
 }
+func TestSLiceFuncForLimit(t *testing.T) {
 
+	val1 := Slicer(9223372036854775807)
+	if val1 != false {
+
+		t.Fatal("failed for positive domain of int 64")
+
+	}
+}
